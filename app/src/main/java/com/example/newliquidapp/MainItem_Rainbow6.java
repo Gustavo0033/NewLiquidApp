8fdfc9a567bd62b043ed753eeef6b790035c59d5
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-09-29 19:19:58 -0300

    Create MainItem_Rainbow6.java

---
+++ app/src/main/java/com/example/newliquidapp/MainItem_Rainbow6.java
@@ -0,0 +1,50 @@
+package com.example.newliquidapp;
+
+public class MainItem_Rainbow6 {
+
+    private int id;
+    private int drawableId;
+    private int Color;
+    private int textStringId;
+
+    public MainItem_Rainbow6(int id, int drawableId,int textStringId, int color) {
+        this.id = id;
+        this.drawableId = drawableId;
+        this.Color = color;
+        this.textStringId = textStringId;
+}
+
+    public void setColor(int color) {
+        Color = color;
+    }
+
+    public void setDrawableId(int drawableId) {
+        this.drawableId = drawableId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTextStringId(int textStringId) {
+        this.textStringId = textStringId;
+    }
+
+    public int getColor() {
+        return Color;
+    }
+
+    public int getDrawableId() {
+        return drawableId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTextStringId() {
+        return textStringId;
+    }
+}
+
+
