ae0b3e34dc78107facebe7518c4289bee0eb92c9
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-04 17:41:40 -0300

    Create players_FreeFire.java

---
+++ app/src/main/java/com/example/newliquidapp/players_FreeFire.java
@@ -0,0 +1,20 @@
+package com.example.newliquidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class players_FreeFire extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players_free_fire);
+
+
+
+
+    }
+}
