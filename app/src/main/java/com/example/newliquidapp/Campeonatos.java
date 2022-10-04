6f89ef877be3838e811454a29c6fd4534e60ff7c
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-04 17:41:14 -0300

    Update Campeonatos.java

--- app/src/main/java/com/example/newliquidapp/Campeonatos.java
+++ app/src/main/java/com/example/newliquidapp/Campeonatos.java
@@ -1,62 +1,116 @@
 package com.example.newliquidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Campeonatos extends AppCompatActivity {
 
-    private View RainbowSix;
-    private View Valorant;
-    private View FreeFire;
-    private View CsGo;
-
+    private RecyclerView Teste;
+    private RecyclerView CampeonatosTL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campeonatos);
 
-        RainbowSix = findViewById(R.id.btn_RainbowSix);
-        RainbowSix.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Campeonatos.this, RainbowSix_Liquid.class);
-                startActivity(intent);
+
+        CampeonatosTL = findViewById(R.id.campeonatosLiquid);
+
+
+       List<InfoCampeonato> InfoCamps = new ArrayList<>();
+       InfoCamps.add(new InfoCampeonato(1,R.drawable.png_fortnite, Color.BLUE));
+       InfoCamps.add(new InfoCampeonato(2,R.drawable.b4d710c6bc6c9009e8b88d9e2326ddec, Color.BLACK));
+       InfoCamps.add(new InfoCampeonato(3, R.drawable.free_fire_logo_12, Color.BLACK));
+
+        CampeonatosTL.setLayoutManager(new LinearLayoutManager(this));
+        adapterCampeonatos adapter = new adapterCampeonatos(InfoCamps);
+        adapter.setListener(id -> {
+            switch (id){
+                case 1:
+                    startActivity(new Intent(Campeonatos.this, CsGo_Liquid.class));
+                    break;
+                case 2:
+                    startActivity(new Intent(Campeonatos.this, Jogadores_Rainbow6.class));
+                    break;
+                case 3:
+                    startActivity(new Intent(Campeonatos.this, players_FreeFire.class));
+                    break;
             }
+
         });
-
-        Valorant = findViewById(R.id.btn_Valorant);
-        Valorant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Campeonatos.this, Valorant_Liquid.class);
-                startActivity(intent);
-            }
-        });
-
-        CsGo = findViewById(R.id.btn_CsGo);
-        CsGo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Campeonatos.this, CsGo_Liquid.class);
-                startActivity(intent);
-            }
-        });
-
-        FreeFire = findViewById(R.id.btn_FreeFire);
-        FreeFire.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Campeonatos.this, FreeFire_Liquid.class);
-                startActivity(intent);
-            }
-        });
-
-
+        CampeonatosTL.setAdapter(adapter);
 
 
 
     }
+
+    private class adapterCampeonatos extends RecyclerView.Adapter<adapterCampeonatos.CampsViewHolder>{
+
+        private List<InfoCampeonato> infoCamps;
+        private clickCampeonatos listener;
+
+        public adapterCampeonatos(List<InfoCampeonato> infoCamps){
+            this.infoCamps = infoCamps;
+        }
+
+        public void setListener(clickCampeonatos listener) {
+            this.listener = listener;
+        }
+
+        @NonNull
+        @Override
+        public CampsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new CampsViewHolder(getLayoutInflater().inflate(R.layout.recycleview_campeonatos,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull CampsViewHolder holder, int position) {
+            InfoCampeonato mainItemCurrent = infoCamps.get(position);
+            holder.bind(mainItemCurrent);
+        }
+
+        @Override
+        public int getItemCount() {
+            return infoCamps.size() ;
+        }
+        private class CampsViewHolder extends RecyclerView.ViewHolder{
+
+            public CampsViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+
+            public void bind(InfoCampeonato item){
+                ImageView imageView = itemView.findViewById(R.id.imageCamps);
+                LinearLayout container = (LinearLayout) itemView;
+
+                container.setOnClickListener(view -> {
+                    listener.onClick(item.getId());
+                });
+
+
+                container.setBackgroundColor(item.getColor());
+                imageView.setImageResource(item.getImageGame());
+
+
+
+            }
+
+        }
+
+
+    }
+
+
 }
