5086007777979825a6e37dd75eb346356d452df8
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-05 09:23:10 -0300

    Create CAPITAO_AMERICCA_MARVEL.java

---
+++ app/src/main/java/com/example/newliquidapp/CAPITAO_AMERICCA_MARVEL.java
@@ -0,0 +1,14 @@
+package com.example.newliquidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CAPITAO_AMERICCA_MARVEL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_capitao_americca_marvel);
+    }
+}
