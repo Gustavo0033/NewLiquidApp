05aaf6a953a7b25365d321ae48715d3a8086fd85
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-05 09:23:52 -0300

    Create MARVEL_HOMEM_ARANHA.java

---
+++ app/src/main/java/com/example/newliquidapp/MARVEL_HOMEM_ARANHA.java
@@ -0,0 +1,88 @@
+package com.example.newliquidapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MARVEL_HOMEM_ARANHA extends AppCompatActivity {
+
+    private RecyclerView SpiderManTL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marvel_homem_aranha);
+
+        SpiderManTL = findViewById(R.id.SpiderManLiquid);
+
+        List<ListSpiderMan> SpiderList = new ArrayList<>();
+        SpiderList.add(new ListSpiderMan(1, R.drawable.tl_07012011892_1280x1920_451x677_139689ba_ae61_4666_ad29_d6778f9cbf3a_451x677, Color.WHITE));
+        SpiderList.add(new ListSpiderMan(2, R.drawable.tl_07012011893_1280x1920_451x677_9cb92800_8bc9_467c_8314_eae7d22d054d_451x677, Color.WHITE));
+        SpiderList.add(new ListSpiderMan(3, R.drawable.tl_07012011927_1280x1920_912x1368_682281d0_99d8_49c6_bb69_ed05af8b2b47_912x1368, Color.WHITE));
+        SpiderList.add(new ListSpiderMan(4, R.drawable.tl_07012011945_1280x1920_912x1368_b9918c96_edd3_439d_962e_8f6864894508_912x1368, Color.WHITE));
+        SpiderList.add(new ListSpiderMan(5, R.drawable.jersey_spider_man_liquid, Color.WHITE));
+        SpiderList.add(new ListSpiderMan(6, R.drawable.marvelxliquid_spider_jersey_02_1280x1920_451x677_d227b281_2a00_4db8_a533_92d9c706840f_451x676, Color.WHITE));
+
+
+
+        SpiderManTL.setLayoutManager(new LinearLayoutManager(this));
+        SpiderAdapter adapter = new SpiderAdapter(SpiderList);
+        SpiderManTL.setAdapter(adapter);
+
+    }
+
+    private class SpiderAdapter extends RecyclerView.Adapter<SpiderViewHolder>{
+
+        private List<ListSpiderMan> SpiderList;
+
+        public SpiderAdapter(List<ListSpiderMan> SpiderList){
+            this.SpiderList = SpiderList;
+        }
+
+        @NonNull
+        @Override
+        public SpiderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new SpiderViewHolder(getLayoutInflater().inflate(R.layout.homem_aranha_recycleview,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SpiderViewHolder holder, int position) {
+            ListSpiderMan mainItemCurrent = SpiderList.get(position);
+            holder.bind(mainItemCurrent);
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return SpiderList.size();
+        }
+    }
+
+    private class SpiderViewHolder extends RecyclerView.ViewHolder{
+
+        public SpiderViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+
+        public void bind(ListSpiderMan item) {
+            ImageView imageView = itemView.findViewById(R.id.img_Spider);
+            LinearLayout container = (LinearLayout) itemView;
+
+            imageView.setImageResource(item.getImageJersey());
+            container.setBackgroundColor(item.getColor());
+
+    }
+
+    }
+}
