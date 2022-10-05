9cc32568f3342a51b1b621dc5a0a125cdf953d8d
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-05 09:23:38 -0300

    Create ListSpiderMan.java

---
+++ app/src/main/java/com/example/newliquidapp/ListSpiderMan.java
@@ -0,0 +1,38 @@
+package com.example.newliquidapp;
+
+public class ListSpiderMan {
+
+    private int id;
+    private int imageJersey;
+    private int Color;
+
+    public ListSpiderMan(int id, int imageJersey, int Color) {
+        this.id = id;
+        this.imageJersey = imageJersey;
+        this.Color = Color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getImageJersey() {
+        return imageJersey;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setImageJersey(int imageJersey) {
+        this.imageJersey = imageJersey;
+    }
+
+    public int getColor() {
+        return Color;
+    }
+
+    public void setColor(int color) {
+        Color = color;
+    }
+}
