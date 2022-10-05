1ee9a3918fe2e78a54b866e6657eecc45fce1550
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-05 09:23:32 -0300

    Update Jogadores_Liquid.java

--- app/src/main/java/com/example/newliquidapp/Jogadores_Liquid.java
+++ app/src/main/java/com/example/newliquidapp/Jogadores_Liquid.java
@@ -1,19 +1,25 @@
 package com.example.newliquidapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.net.Inet4Address;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Jogadores_Liquid extends AppCompatActivity {
 
-    private View JogadoresR6;
-    private View JogadorasValorant;
-    private View JogadoresFreeFire;
-    private View JogadoresCsGO;
+   private RecyclerView PlayersLiquid;
 
 
     @Override
@@ -22,44 +28,5 @@ public class Jogadores_Liquid extends AppCompatActivity {
         setContentView(R.layout.activity_jogadores_liquid);
 
 
-
-        JogadoresR6 = findViewById(R.id.btn_Jogadores_Rainbow6);
-        JogadoresR6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Jogadores_Liquid.this, Jogadores_Rainbow6.class);
-                startActivity(intent);
-            }
-        });
-
-
-        JogadorasValorant = findViewById(R.id.btn_Jogadores_Valorant);
-        JogadorasValorant.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Jogadores_Liquid.this, Jogadoras_Valorant.class);
-                startActivity(intent);
-            }
-        });
-
-
-        JogadoresFreeFire = findViewById(R.id.btn_Jogadores_FreeFire);
-        JogadoresFreeFire.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Jogadores_Liquid.this,players_FreeFire.class);
-            startActivity(intent);}
-        });
-
-        JogadoresCsGO = findViewById(R.id.btn_Jogadores_CsGo);
-        JogadoresCsGO.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Jogadores_Liquid.this, CsGo_Liquid.class);
-                startActivity(intent);
-            }
-        });
-
-
-    }
+}
 }
