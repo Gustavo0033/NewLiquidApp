f18a8ae196e9ae0002c80e31674450d8bd483c1e
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-05 09:24:01 -0300

    Create PlayersTL.java

---
+++ app/src/main/java/com/example/newliquidapp/PlayersTL.java
@@ -0,0 +1,37 @@
+package com.example.newliquidapp;
+
+public class PlayersTL {
+
+    private int id;
+    private int imagePlayers;
+    private int Color;
+
+    public PlayersTL(int id, int imagePlayers, int Color) {
+        this.id = id;
+        this.imagePlayers = imagePlayers;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getImagePlayers() {
+        return imagePlayers;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setImagePlayers(int imagePlayers) {
+        this.imagePlayers = imagePlayers;
+    }
+
+    public int getColor() {
+        return Color;
+    }
+
+    public void setColor(int color) {
+        Color = color;
+    }
+}
