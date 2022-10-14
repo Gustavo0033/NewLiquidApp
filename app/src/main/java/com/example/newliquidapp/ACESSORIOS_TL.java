3d865d37fa11ab94d54aec2419823cc6edded484
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-14 18:29:50 -0300

    Create ACESSORIOS_TL.java

---
+++ app/src/main/java/com/example/newliquidapp/ACESSORIOS_TL.java
@@ -0,0 +1,15 @@
+package com.example.newliquidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ACESSORIOS_TL extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_acessorios_tl);
+    }
+}
