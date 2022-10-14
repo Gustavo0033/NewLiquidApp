891bdb2755165f271c93f5e700809ab7b3b5b9f7
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-14 18:30:51 -0300

    Create THE_BLUE_CAPSULE.java

---
+++ app/src/main/java/com/example/newliquidapp/THE_BLUE_CAPSULE.java
@@ -0,0 +1,14 @@
+package com.example.newliquidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class THE_BLUE_CAPSULE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_blue_capsule);
+    }
+}
