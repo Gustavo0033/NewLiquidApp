3cb951999d81ccad859508cc5ddd7b93dc420720
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-28 12:56:34 -0300

    LiquidPlus click embaixo

---
+++ app/src/main/java/com/example/newliquidapp/Experience_LiquidPlus.java
@@ -0,0 +1,15 @@
+package com.example.newliquidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Experience_LiquidPlus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_experience_liquid_plus);
+    }
+}
