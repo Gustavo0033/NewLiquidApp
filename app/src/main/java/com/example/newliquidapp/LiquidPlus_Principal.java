ebf48ccf3d788455939782554b41d2e63f37ba1f
Gustavo Mendonça do Nascimento <102833033+Gustavo0033@users.noreply.github.com>
2022-10-28 12:56:34 -0300

    LiquidPlus click embaixo

--- app/src/main/java/com/example/newliquidapp/LiquidPlus_Principal.java
+++ app/src/main/java/com/example/newliquidapp/LiquidPlus_Principal.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,12 +15,20 @@ import java.util.List;
 
 public class LiquidPlus_Principal extends AppCompatActivity {
 
+        private View experiencePlus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_liquid_plus_principal);
+
+        experiencePlus = findViewById(R.id.Experience);
+        experiencePlus.setOnClickListener(view -> {
+            Intent intent = new Intent(LiquidPlus_Principal.this, Experience_LiquidPlus.class);
+            startActivity(intent);
+        });
 
     }
 
